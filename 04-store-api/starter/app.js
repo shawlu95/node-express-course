95152bd5e29fc0020f3468cfe0531d7546c537e4
shaw.lu Xiao Lu <shaw.lu@coupang.com>
2022-02-16 16:23:03 -0800

    Start project 4

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require("express");
+const connectDB = require("./db/connect");
+const products = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const { application } = require('express');
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("OK")
+});
+
+app.use("/api/v1/products", products);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        // connectDB
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening port ${port}...`));
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+start();
